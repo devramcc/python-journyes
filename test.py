77aacd7f8eb62b94a47df29201cfdaaa062da85e
devramcc <dev.ramddd@gmail.com>
2023-04-09 19:50:30 +0700

    add git ignore

--- test.py
+++ test.py
@@ -1,9 +1,42 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_iris
 import numpy as np
 
-def mean_naive(X):
-    return np.mean(X, axis = 1, keepdims=True)
+# Define the function that creates the Keras model with the hyperparameters to tune
+def create_model(hidden_layer_size=32, learning_rate=0.001):
+    model = keras.Sequential([
+        keras.layers.Dense(hidden_layer_size, activation='relu', input_shape=(4,)),
+        keras.layers.Dense(3, activation='softmax')
+    ])
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(optimizer=optimizer,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
 
-X = np.array([[0., 1., 1.], 
-              [1., 2., 1.]])
+# Load the iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
 
-print(mean_naive(X))
+# Define the hyperparameters to tune
+param_grid = {
+    'hidden_layer_size': [16, 32, 64],
+    'learning_rate': [0.01, 0.001, 0.0001]
+}
+
+# Create a KerasClassifier model with the create_model function and the hyperparameters to tune
+model = KerasClassifier(build_fn=create_model, epochs=20, batch_size=5, verbose=0)
+
+# Create a GridSearchCV object with the hyperparameters to tune and the model to use
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+
+# Fit the GridSearchCV object to the data
+grid_search.fit(X, y)
+
+# Print the best hyperparameters and the corresponding accuracy score
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best score: {grid_search.best_score_}")
